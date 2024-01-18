32cead96e937a7f038031aa2f597cf6309c51c18
shayarbiv <shay.arbiv@riskified.com>
2024-01-18 20:00:37 +0200

    app 0.0.3

--- common/images.py
+++ common/images.py
@@ -2,21 +2,21 @@ import subprocess
 import os
 
 
-def combo_trim_split(img, output_path, image_vars, in_place=None):
-    trim_borders(img, output_path, image_vars)
+def combo_trim_split(img, output_path, fuzz, in_place=None):
+    trim_borders(img, output_path, fuzz)
     img = os.path.join(output_path, os.path.basename(img))
-    vertical_split(img, output_path, image_vars,True)
+    vertical_split(img, output_path, fuzz, True)
 
 
-def trim_borders(img, output_path, image_vars, in_place=None):
-    command = f'mogrify -fuzz {image_vars["fuzz"]}% -trim -path {output_path} {img}'
+def trim_borders(img, output_path, fuzz, in_place=None):
+    command = f'mogrify -fuzz {fuzz}% -trim -path {output_path} {img}'
     subprocess.run(command.split(' '), stdout=subprocess.PIPE, text=True)
     return
 
 
-def vertical_split(img, output_path, image_vars, in_place=False):
+def vertical_split(img, output_path, fuzz=None, in_place=False):
     in_place = '' if in_place else f'-path {output_path} '
-    command = f'mogrify -crop {image_vars["split"]}%x100% +repage {in_place}{img}'
+    command = f'mogrify -crop 50%x100% +repage {in_place}{img}'
     subprocess.run(command.split(' '), stdout=subprocess.PIPE, text=True)
     return
 
--- pages/Image_Actions.py
+++ pages/Image_Actions.py
@@ -28,16 +28,11 @@ action = st.radio("Choose an action", ['Trim Borders', 'Vertical Split', 'Trim &
                   horizontal=True, key='action_radio')
 images_to_display = []
 captions = []
-working_dir = os.getcwd()
-image_vars = {}
+output_path = os.getcwd()
+fuzz = None
 if st.session_state.action_radio:
-    if action == 'Trim Borders':
-        image_vars['fuzz'] = st.slider('How much fuzz?', 0, 100, 20)
-    elif action == 'Vertical Split':
-        image_vars['split'] = st.slider('Where to split?', 0, 100, 50)
-    else:
-        image_vars['fuzz'] = st.slider('How much fuzz?', 0, 100, 20)
-        image_vars['split'] = st.slider('Where to split?', 0, 100, 50)
+    if action in ['Trim Borders', 'Trim & Split Combo']:
+        fuzz = st.slider('How much fuzz?', 0, 100, 20)
     action = action.lower().replace(" ", "_")
     files = image_file_uploads(action)
     if files:
@@ -48,13 +43,13 @@ if st.session_state.action_radio:
                 with TemporaryDirectory() as temp_dir:
                     temp_file_path = os.path.join(temp_dir, file.name)
                     save_uploaded_file(temp_dir, file)
-                    FUNC_MAP[action](temp_file_path, working_dir, image_vars, None)
+                    FUNC_MAP[action](temp_file_path, output_path, fuzz, None)
                 if action == 'trim_borders':
-                    images_to_display.append(os.path.join(working_dir, file.name))
+                    images_to_display.append(os.path.join(output_path, file.name))
                     captions.append(file.name)
                 else:
-                    images_to_display.extend([os.path.join(working_dir, f'{_}-0{file_ext}'),
-                                              os.path.join(working_dir, f'{_}-1{file_ext}')])
+                    images_to_display.extend([os.path.join(output_path, f'{_}-0{file_ext}'),
+                                              os.path.join(output_path, f'{_}-1{file_ext}')])
                     captions.extend([f'{_}-0{file_ext}', f'{_}-0{file_ext}'])
             st.success(f'Saved results in output folder')
             if len(files) <= 2:
